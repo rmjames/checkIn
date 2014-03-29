9adcd11710d91d0e7b44c73b16da378358a7995f
gtrmjz@gmail.com <gtrmjz@gmail.com>
2014-03-29 17:44:34 -0400

    1st

---
+++ app/scripts/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$('.trigger').on('click', function(){
+			$('.nav').toggleClass('slideNav');
+		});
+	$([href='/settings.html']).on('click', function(){
+		$('.settings').toggleClass('gn');
+		});	
+		
+			});
+	});
